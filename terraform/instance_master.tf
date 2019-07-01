9d990f43a6efda7994ecac55a5b8f19d92531e46
mat-kainos <a@a.com>
2019-07-01 16:43:27 +0200

    + public ip

--- terraform/instance_master.tf
+++ terraform/instance_master.tf
@@ -3,8 +3,10 @@ resource "aws_instance" "jenkins_master" {
   ami                    = "${data.aws_ami.jenkins-master.id}"
   instance_type          = "${var.jenkins_master_instance_type}"
   key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${var.sec_group}"]
   subnet_id              = "${element(var.vpc_private_subnets, 0)}"
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${var.sec_group}"]
+
   root_block_device {
     volume_type           = "gp2"
     volume_size           = 8
